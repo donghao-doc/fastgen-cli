4eedf03fab01f7cfb34d7c2cfbb6906c47b5991b
BarryDong <286084845@qq.com>
2025-07-16 16:08:51 +0800

    feat: 项目模板添加分支

--- src/templates.ts
+++ src/templates.ts
@@ -3,15 +3,17 @@ export const templates = {
     vue: [
       {
         name: 'Vben Admin',
-        repo: 'https://github.com/vbenjs/vue-vben-admin.git',
         description: '基于 Vue3、Pinia、Vue Router、TypeScript 等最新技术栈的管理后台模板',
+        repo: 'https://github.com/vbenjs/vue-vben-admin.git',
+        branch: 'main',
       }
     ],
     react: [
       {
         name: 'Slash Admin',
-        repo: 'https://github.com/d3george/slash-admin.git',
         description: '基于 React18、Vite、TypeScript、Ant Design 5.0 等最新技术栈的管理后台模板',
+        repo: 'https://github.com/d3george/slash-admin.git',
+        branch: 'main',
       }
     ]
   },
@@ -19,13 +21,15 @@ export const templates = {
     vue: [
       {
         name: 'Vue-H5-Template',
-        repo: 'https://github.com/sunniejs/vue-h5-template.git',
         description: '基于 vite3 + vue3 + pinia + Vant + viewport 适配方案的移动端快速开发脚手架',
+        repo: 'https://github.com/sunniejs/vue-h5-template.git',
+        branch: 'vue-h5-template',
       },
       {
         name: 'vue3-vant-mobile',
-        repo: 'https://github.com/vue-zone/vue3-vant-mobile.git',
         description: '基于 Vue 3 生态系统的移动 web 应用模板',
+        repo: 'https://github.com/vue-zone/vue3-vant-mobile.git',
+        branch: 'main',
       }
     ]
   },
@@ -33,8 +37,9 @@ export const templates = {
     uniapp: [
       {
         name: 'uni-best',
-        repo: 'https://github.com/codercup/unibest.git',
         description: '由 uniapp + Vue3 + Ts + Vite4 + UnoCss + VSCode + uni插件+ wot-ui 构建的最佳实践模板',
+        repo: 'https://github.com/codercup/unibest.git',
+        branch: 'main',
       }
     ]
   }
